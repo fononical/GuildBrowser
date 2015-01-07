ad04bff90ee249ade900d26d1ed73efcb420a0d4
Alain Bustamante <alainbustamante@gmail.com>
2015-01-06 19:18:00 -0800

    This is my commitment since a while. (Tuesday, January 6th, 2015)

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Alain Bustamante on 6/12/14.
+//  Copyright (c) 2014 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
---
+++ GuildBrowserLogicTests/WowUtilsTests.m
@@ -0,0 +1,14 @@
+//
+//  WowUtilsTests.m
+//  GuildBrowser
+//
+//  Created by Alain Bustamante on 6/12/14.
+//  Copyright (c) 2014 Charlie Fulton. All rights reserved.
+//
+
+#import "WowUtilsTests.h"
+#import "WoWUtils.h"
+
+@implementation WowUtilsTests
+
+@end
